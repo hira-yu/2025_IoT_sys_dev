2ebb8e6c806e977d558d0be690134d0a54c426cd
Choco2807 <yuya_hirayama_g0121@icloud.com>
2025-07-18 15:12:43 +0900

    v1.2

--- api/index.js
+++ api/index.js
@@ -44,31 +44,21 @@ router.get( "/", async ( req, res ) => {
             });
             break;
 
-        /*case "temp":
-            var obniz = new Obniz( "9964-7739" );
-            var connect = await obniz.connectWait({ timeout: 10 });
-
-            if( connect ) {
-                let temp_sensor = obniz.wired( "Keyestudio_TemperatureSensor", { signal: 0, vcc: 1, gnd: 2 });
-                await obniz.wait( 100 );
-                let temp = await temp_sensor.getWait( );
-
-                obniz.close( );
-                res.json({ status: 200, temp: temp });
-            } else {
-                res.json({ status: 503 });
-            }
-            break;*/
-
         case "temps":
             var obniz = new Obniz( "9964-7739" );
             var connect = await obniz.connectWait({ timeout: 10 });
 
             if( connect ) {
+                await obniz.wait( 500 );
                 try {
                     obniz.i2c0.start({ mode: "master", gnd: 8, sda: 10, scl: 11, clock: 400000 });
-                    obniz.i2c0.write( 0x44, [ 0xFD ]);
+                    obniz.i2c0.onerror = ( err ) => {
+                        console.log( err );
+                    }
                     await obniz.wait( 100 );
+                    obniz.i2c0.write( 0x44, [ 0xFD ]);
+                    await obniz.wait( 20 );
+
                     var data = await obniz.i2c0.readWait( 0x44, [ 6 ]);
 
                     t_ticks = ( data[ 0 ] << 8 ) + data[ 1 ];
@@ -104,6 +94,7 @@ router.get( "/", async ( req, res ) => {
             if( connect ) {
                 try {
                     let ir_module = obniz.wired( "IRModule", { vcc: 0, send: 1, recv: 2, gnd: 3 });
+                    await obniz.wait( 100 );
                     let ir_data = null;
                     ir_module.cutTail = true;
 
@@ -122,7 +113,7 @@ router.get( "/", async ( req, res ) => {
                 res.json({ status: 503 });
             }
             break;
-        
+
         case "ir_send":
             var obniz = new Obniz( "9964-7739" );
             var connect = await obniz.connectWait({ timeout: 20 });
@@ -146,25 +137,6 @@ router.get( "/", async ( req, res ) => {
 
 router.post( "/", async ( req, res ) => {
     switch( req.query.post ){
-        case "set_ir":
-            /*
-            fs.writeFile( __dirname + "/json/write_test.json", JSON.stringify( req.body ), ( err ) => {
-                if( err ) throw err;
-            });
-
-            console.log( req.body );*/
-
-            var py_script = new PythonShell( './py/ir_analy.py' );
-
-            py_script.send( req.body["data"][0] );
-            //py_script.send( ["test", "test2"] );
-
-            py_script.on( "message",function(message) {
-                console.log(message);
-            });
-
-            res.sendStatus( 200 );
-        
         case "ir_send":
             var py_script = new PythonShell( './py/ir_analy.py' );
             py_script.send( req.body[ "data" ][ 0 ] );
@@ -175,15 +147,25 @@ router.post( "/", async ( req, res ) => {
 
                 if( connect ) {
                     try {
+                        data = JSON.parse( fs.readFileSync( __dirname + "/json/config.json", "utf-8" ));
+
                         let ir_module = obniz.wired( "IRModule", { vcc: 0, send: 1, recv: 2, gnd: 3 });
 
-                        ir_module.send( message.slice( 1, -1 ).split(", ").map( Number ));
+                        if( !( data.air.toString( ) == ( req.body["data"][ 0 ].split( "," ).slice( 1, 9 )).toString( ))) {
+                            ir_module.send( message.slice( 1, -1 ).split(", ").map( Number ));
+                            await obniz.wait( 200 );
 
+                            data.air = req.body[ "data" ][ 0 ].split( "," ).slice( 1, 9 );
+                            data.air[ 4 ] = Number( data.air[ 4 ]);
+
+                            fs.writeFile( __dirname + "/json/config.json", JSON.stringify( data, null, 4 ), ( err ) => {
+                                if( err ) throw err;
+                            });
+                        }
                     } finally {
                         obniz.close( );
                     }
-
-                }else {
+                } else {
                     obniz.close( );
                     res.sendStatus( 503 );
                     return;
--- public/js/main.js
+++ public/js/main.js
@@ -7,26 +7,42 @@
 const weekday = [ "日","月","火","水","木","金","土" ];
 var forecast, warnings, config = { };
 
-$.getJSON( './api/?req=config', ( data ) => {
+const get_config = $.getJSON( './api/?req=config', ( data ) => {
     config = data;
 });
 
-$.getJSON( './api/?req=forecasts', ( data ) => {
+const get_forecasts = $.getJSON( './api/?req=forecasts', ( data ) => {
     forecast = data[ "forecasts" ][ 0 ];
 });
 
-$.getJSON( './api/?req=warnings', ( data ) => {
+const get_warnings = $.getJSON( './api/?req=warnings', ( data ) => {
     warnings = data[ "warnings" ][ 0 ];
-    render_weather( );
-    get_temps( );
+});
+
+Promise.allSettled(
+    [ get_config, get_forecasts, get_warnings ]
+).then(( ) => {
+    initialize_remote_control( );
+    main_loop( );
 });
 
 function render_weather( ) {
     var weather = get_weather( config.area.pref, config.area.city );
+
     weather.then(( res ) => {
-        console.log( res );
         let current_date = new Date( );
         let today_date = new Date( current_date ).setHours( 0, 0, 0, 0 );
+
+        // html initialize
+        $( "#today_temp_min" ).text( "--" );
+        $( "#today_temp_max" ).text( "--" );
+        $( "#tomorrow_temp_min" ).text( "--" );
+        $( "#tomorrow_temp_max" ).text( "--" );
+        $( "#today_pops > tbody > tr > td:nth-of-type(1)" ).text( "--" );
+        $( "#today_pops > tbody > tr > td:nth-of-type(2)" ).text( "--" );
+        $( "#today_pops > tbody > tr > td:nth-of-type(3)" ).text( "--" );
+        $( "#tomorrow_pops > tbody > tr > td:nth-of-type(1)" ).text( "--" );
+        $( "#tomorrow_pops > tbody > tr > td:nth-of-type(2)" ).text( "--" );
 
         $( "#today_weather_icon" ).attr( "alt", res[ 0 ][ 1 ][ 0 ]);
         $( "#tomorrow_weather_icon" ).attr( "alt", res[ 0 ][ 1 ][ 1 ]);
@@ -47,14 +63,16 @@ function render_weather( ) {
         for( var i = 0; i < res[ 1 ][ 1 ].length; i++ ) {
             if( res[ 1 ][ 1 ][ i ].status != "解除" ) {
                 let warning = res[ 1 ][ 1 ][ i ].code;
-                warning_elems += `<p class = "waring_label level_${ warning / 10 | 0 }">${ warnings[ warning ]}</p>`;
+                warning_elems += `<p class = "warning-label level_${ warning / 10 | 0 }">${ warnings[ warning ]}</p>`;
             }
         }
+
+        $( "#warnings" ).text( "" );
 
         if( 0 < warning_elems.length ) {
             $( "#warnings" ).append( warning_elems );
         } else {
-            $( "#warnings" ).append( `<p class = "waring_label">発令中の警報・注意報はありません</p>` )
+            $( "#warnings" ).append( `<p class = "warning-label">発令中の警報・注意報はありません</p>` )
         }
 
         let forecast_current_dates = [ new Date( today_date ).setHours( -5, 0, 0, 0 )];
@@ -72,11 +90,18 @@ function render_weather( ) {
                 case forecast_current_dates[ 0 ]:
                     if( i == 0 ) {
                         $( "#today_temp_min" ).text( `${ res[ 0 ][ 2 ][ i ]}` );
+                        if( !( 5 <= current_date.getHours( ) && current_date.getHours( ) <= 15 )){
+                            $( "#tomorrow_temp_min" ).text( `${ res[ 0 ][ 2 ][ i ]}` );
+                        }
                     }
                     break;
 
                 case forecast_current_dates[ 1 ]:
-                    $( "#today_temp_max" ).text( `${ res[ 0 ][ 2 ][ i ]}` );
+                    if( !( 5 <= current_date.getHours( ) && current_date.getHours( ) <= 15 )){
+                        $( "#tomorrow_temp_max" ).text( `${ res[ 0 ][ 2 ][ i ]}` );
+                    } else {
+                        $( "#today_temp_max" ).text( `${ res[ 0 ][ 2 ][ i ]}` );
+                    }
                     break;
 
                 case forecast_current_dates[ 2 ]:
@@ -117,11 +142,19 @@ function render_weather( ) {
                     break;
 
                 case pops_current_dates[ 1 ]:
-                    $( "#today_pops > tbody > tr > td:nth-of-type(2)").text( `${ res[ 0 ][ 3 ][ i ] }%` );
+                    if( 5 <= current_date.getHours( ) && current_date.getHours( ) <= 15 ){
+                        $( "#today_pops > tbody > tr > td:nth-of-type(2)").text( `${ res[ 0 ][ 3 ][ i ] }%` );
+                    } else {
+                        $( "#tomorrow_pops > tbody > tr > td:nth-of-type(1)").text( `${ res[ 0 ][ 3 ][ i ] }%` );
+                    }
                     break;
 
                 case pops_current_dates[ 2 ]:
-                    $( "#today_pops > tbody > tr > td:nth-of-type(3)").text( `${ res[ 0 ][ 3 ][ i ] }%` );
+                    if( 5 <= current_date.getHours( ) && current_date.getHours( ) <= 15 ){
+                        $( "#today_pops > tbody > tr > td:nth-of-type(3)").text( `${ res[ 0 ][ 3 ][ i ] }%` );
+                    } else {
+                        $( "#tomorrow_pops > tbody > tr > td:nth-of-type(2)").text( `${ res[ 0 ][ 3 ][ i ] }%` );
+                    }
                     break;
 
                 case pops_current_dates[ 3 ]:
@@ -150,6 +183,10 @@ function get_date( num ) {
 
 function calc_THI( T, H ) {
     return ( 0.81 * T + 0.01 * H * ( 0.99 * T - 14.3 ) + 46.3 ).toFixed( 2 );
+}
+
+function recalc_THI( H ) {
+    return Math.round(( 158.8 * H + 26500 ) / ( 900 + 11 * H ));
 }
 
 function calc_WBGT( T, H ) {
@@ -183,14 +220,11 @@ function get_temps( ) {
 
         $( "#monitor_temp" ).text( res.temp );
         $( "#monitor_hum" ).text( res.hum );
-        $( "#monitor_THI" ).text( calc_THI( res.temp, res.hum ));
-        $( "#monitor_WBGT" ).text( calc_WBGT( res.temp, res.hum ));
-        $( "#THI_box > span:nth-of-type( 3 )" ).text( thi[ 0 ]);
-        $( "#WBGT_box > span:nth-of-type( 3 )" ).text( wbgt[ 0 ]);
+        $( "#monitor_THI" ).text( thi[ 0 ]);
+        $( "#monitor_WBGT" ).text( wbgt[ 0 ]);
 
-
-        $( "#THI_box > span:nth-of-type( 3 )" ).addClass( `attn_${ thi[ 1 ]}` );
-        $( "#WBGT_box > span:nth-of-type( 3 )" ).addClass( `attn_${ wbgt[ 1 ] * 2 + 2 }` );
+        $( "#THI_box" ).addClass( `attn_${ thi[ 1 ]}` );
+        $( "#WBGT_box" ).addClass( `attn_${ wbgt[ 1 ] * 2 + 2 }` );
     });
 }
 
@@ -214,19 +248,100 @@ async function get_ir( ) {
     return data;
 }
 
-$( document ).ready( function( ){
-    $( "#ir_test" ).click( async function( ){
-        /*var data = await get_ir( );
+function render_air_info( ){
+    let power       = [ "on", "off" ];
+    let power_label = [ "入", "切" ];
 
-        $.post( "/api?post=set_ir", `data[]=${[ data.data ]}`, ( err ) => {
-            if( err ) throw err;
-        });*/
-        let data = [ "generate", "on", "dry", "none", "", 25, "1", "1", "1" ];
-        $.post( "/api?post=ir_send", `data[]=${ data }`, ( err ) => {
-            if( err ) throw err;
-        });
+    let mode       = [ "heat", "cool", "dry", "dry_low", "auto" ];
+    let mode_label = [ "暖房", "冷房", "除湿", "除湿（弱）", "自動" ];
+    let mode_imgs  = [ "./img/air/heat.png", "./img/air/cool.png", "./img/air/dry.png", "./img/air/dry_low.png", "./img/air/auto.png", "./img/air/off.png" ]
+
+    let wind_power       = [ "auto", "silent", "1", "2", "3", "4", "powerful" ];
+    let wind_power_label = [ "自動", "静か", "弱", "中", "中強", "強", "パワフル" ];
+
+    let wind_dir_v       = [ "auto", "1", "2", "3", "4", "5" ]
+    let wind_dir_v_label = [ "自動", "最上", "上", "中央", "下", "最下" ];
+
+    let wind_dir_s       = [ "auto", "1", "2", "3", "4", "5" ];
+    let wind_dir_s_label = [ "自動", "最左", "左", "中央", "右", "最右" ];
+
+    var power_val = power.indexOf( config.air[ 0 ]);
+    var mode_val = mode.indexOf( config.air[ 1 ]);
+    var temp_val = Number( config.air[ 4 ]);
+    var wind_power_val = wind_power.indexOf( config.air[ 5 ]);
+    var wind_dir_s_val = wind_dir_s.indexOf( config.air[ 6 ]);
+    var wind_dir_v_val = wind_dir_v.indexOf( config.air[ 7 ]);
+
+    let data = [ "generate", power[ power_val ], mode[ mode_val ], "none", "", temp_val, wind_power[ wind_power_val ], wind_dir_s[ wind_dir_s_val ], wind_dir_v[ wind_dir_v_val ]];
+
+    $.post( "/api?post=ir_send", `data[]=${ data }`, ( err ) => {
+        if( err != "OK" ) throw err;
+
+        $( "#power" ).text( "--" );
+        $( "#temp" ).text( "--" );
+        $( "#wind_power" ).text( "--" );
+        $( "#wind_dir_v" ).text( "--" );
+        $( "#wind_dir_s" ).text( "--" );
+
+        if( power_val == 1 ) {
+            $( "#power" ).text( power_label[ power_val ]);
+            $( "#air_status_img" ).attr( "src", mode_imgs[ 5 ]);
+
+        } else {
+            $( "#air_status_img" ).attr( "src", mode_imgs[ mode_val ]);
+            $( "#power" ).text( power_label[ power_val ] + "（" + mode_label[ mode_val ] + "）");
+            $( "#temp" ).text( temp_val );
+            $( "#wind_power" ).text( wind_power_label[ wind_power_val ]);
+            $( "#wind_dir_v" ).text( wind_dir_v_label[ wind_dir_v_val ]);
+            $( "#wind_dir_s" ).text( wind_dir_s_label[ wind_dir_s_val ]);
+        }
     });
-});
+
+    update_remote_ui( );
+}
+
+function air_ctrl( ) {
+    let hours = new Date( ).getHours( );
+    var temp = Number( $( "#monitor_temp" ).text( ));
+    var hum  = Number( $( "#monitor_hum"  ).text( ));
+    var month = Number( get_date( 0 ).split( "月" )[ 0 ]);
+
+    if( Number.isNaN( temp )  || Number.isNaN( hum )) {
+        return
+    }
+
+    if( 12 <= month || month <= 1 ) {
+        config.air[ 1 ] = "auto";
+
+    } else {
+        if( 50 <= hum ) {
+            config.air[ 1 ] = "dry";
+        } else {
+            config.air[ 1 ] = "cool";
+        }
+
+        if( 5 <= hours && hours < 7 ) {
+            var fcst_temp = Number( $( "#today_temp_max" ).text( ));
+            var fcst_pop  = Number( $( "#today_pops > tbody > tr > td:first-child" ).text( ).slice( 0, -1 ));
+            config.air[ 0 ] = "on";
+
+            if( 30 <= fcst_temp ) {
+                config.air[ 1 ] = "cool";
+                config.air[ 4 ] = 26;
+
+            } else if( 70 <= fcst_pop ) {
+                config.air[ 1 ] = "dry";
+                config.air[ 4 ] = 27;
+
+            } else {
+                config.air[ 4 ] = recalc_THI( hum );
+            }
+
+        } else {
+            config.air[ 4 ] = recalc_THI( hum );
+        }
+    }
+}
 
 function decision_THI( num ) {
     var thi_str = [ "寒くてたまらない", "寒い", "肌寒い", "何も感じない", "快適", "暑くない", "やや暑い", "暑くて汗が出る", "暑くてたまらない" ];
@@ -234,7 +349,7 @@ function decision_THI( num ) {
 
     if( 49.99 < num ) {
         index = Math.ceil(( num - 49.99 ) / 5 );
-    
+
         if( thi_str.length <= index ) {
             index = thi_str.length - 1;
         }
@@ -257,3 +372,91 @@ function decision_WBGT( num ){
 
     return [ wbgt_str[ index ], index ];
 }
+
+function update_remote_ui() {
+    const is_power_on = config.air[ 0 ] === 'on';
+
+    $( '#remote_power' ).toggleClass( 'active', is_power_on );
+
+    $( '.remote-body button:not(#remote_power)' ).prop( 'disabled', !is_power_on );
+    $( '.remote-body .remote-section:not(:first-child)' ).css( 'opacity', is_power_on ? 1 : 0.5 );
+
+    if ( is_power_on ) {
+        $( '.mode-btn' ).removeClass( 'active' );
+        $( `.mode-btn[ data-mode = "${ config.air[ 1 ]}"]` ).addClass( 'active' );
+
+        $( '#remote_temp_display' ).text( config.air[ 4 ] + '°C' );
+
+        $( '.wind-btn' ).removeClass( 'active' );
+        $( `.wind-btn[ data-wind = "${ config.air[ 5 ]}"]` ).addClass( 'active' );
+
+        $( '.wind-dir-s-btn' ).removeClass( 'active' );
+        $( `.wind-dir-s-btn[ data-wind-s = "${ config.air[ 6 ]}"]` ).addClass( 'active' );
+
+        $( '.wind-dir-v-btn' ).removeClass( 'active' );
+        $( `.wind-dir-v-btn[ data-wind-v = "${ config.air[ 7 ]}"]` ).addClass( 'active' );
+    }
+}
+
+function initialize_remote_control( ) {
+    Promise.all(
+        [ get_config ]
+    ).then(( ) => {
+        update_remote_ui( );
+    });
+
+    $( '#remote_power' ).on( 'click', function( ) {
+        config.air[ 0 ] = ( config.air[ 0 ] === 'on' ) ? 'off' : 'on';
+        update_remote_ui( );
+        render_air_info( );
+    });
+
+    $( '.mode-btn' ).on( 'click', function( ) {
+        config.air[ 1 ] = $( this ).data( 'mode' );
+        update_remote_ui( );
+    });
+
+    $( '#remote_temp_up' ).on( 'click', function( ) {
+        let temp = parseInt( config.air[ 4 ]);
+        if ( temp < 30 ) config.air[ 4 ] = temp + 1;
+        update_remote_ui( );
+    });
+
+    $( '#remote_temp_down' ).on( 'click', function( ) {
+        let temp = parseInt( config.air[ 4 ]);
+        if ( temp > 16 ) config.air[ 4 ] = temp - 1;
+        update_remote_ui( );
+    });
+
+    $( '.wind-btn' ).on( 'click', function( ) {
+        config.air[ 5 ] = String( $( this ).data( 'wind' ));
+        update_remote_ui( );
+    });
+
+    $( '.wind-dir-s-btn' ).on( 'click', function( ) {
+        config.air[ 6 ] = String( $( this ).data( 'wind-s' ));
+        update_remote_ui( );
+    });
+
+    $( '.wind-dir-v-btn' ).on( 'click', function( ) {
+        config.air[ 7 ] = String( $( this ).data( 'wind-v' ));
+        update_remote_ui( );
+    });
+
+    $( '#remote_submit' ).on( 'click', function( ) {
+        render_air_info( );
+    });
+}
+
+async function main_loop( ){
+    await render_weather( );
+    await get_temps( );
+
+    air_ctrl( );
+
+    setTimeout(( ) => {
+        render_air_info( );
+    }, 5000 );
+
+    setTimeout( main_loop, 30000 );
+};
